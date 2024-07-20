652d6d9f46f7d33b8901b4188e8f31e023dd8382
mwangee <mwangicollins022@gmail.com>
2024-07-20 20:13:30 +0300

    Add files via upload

---
+++ basic calculator/script.js
@@ -0,0 +1,9 @@
+numberButtons = document.querySelectorAll('[data-number]')
+operationsButtons = document.querySelectorAll('[data-operation]')
+deleteButton = document.querySelectorAll('[data-delete]')
+allClearButton = document.querySelectorAll('[data-all-clear]')
+previousOperandButton = document.querySelectorAll('[data-previous-operand]')
+currentOperandButton = document.querySelectorAll('[data-current-operand]')
+equalsButton = document.querySelectorAll('[data-equals]')
+
+console.log("Hello World");
